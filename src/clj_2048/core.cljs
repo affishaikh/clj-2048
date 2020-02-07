97980a355a265cb455f730430ccc47637ee03c79
affishaikh <aftabshk@In-Aftabshk.local>
2020-02-08 00:09:09 +0530

    Refactor board and move-left

--- src/clj_2048/core.cljs
+++ src/clj_2048/core.cljs
@@ -1,32 +1,32 @@
 (ns clj-2048.core
-    (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 (defonce board-state (atom [[0 2 0 2] [2 0 2 0] [2 2 2 2] [2 0 0 2]]))
 
 (defn move-left
   [board]
-  (loop
-   [b (remove #{0} board) res []]
-    (cond
-      (empty? b)
-       (concat res (repeat (- 4 (count res)) 0))
-      (= (first b) (second b))
-       (recur (nnext b) (conj res (+ (first b) (second b))))
-      :else
-        (recur (next b) (conj res (first b))))))
+  (map
+    #(as-> %1 r
+           (remove zero? r)
+           (partition-by identity r)
+           (mapcat (partial partition-all 2 2) r)
+           (map (partial apply +) r)
+           (concat r (repeat (- 4 (count r)) 0))
+           )
+    board))
 
 (defn board
   []
-   [:table
-    {:class ["board"] :autofocus 1 :tabindex 1
-     :on-key-down (fn [] (swap! board-state #(map move-left @board-state)))}
-    [:tbody
-     (map
-       (fn [r]
-         [:tr
-          (map (fn [d]
-                 [:td d]) r)])
-       @board-state)]])
+  [:div
+   {:class ["board"] :autofocus 1 :tabindex 1
+    :on-key-down (fn [] (swap! board-state move-left))}
+   (map
+     (fn [row]
+       [:div
+        {:class ["row"]}
+        (map (fn [d] [:div
+                      {:class ["num"]} (if (zero? d) "" d)]) row)])
+     @board-state)])
 
 (reagent/render-component [board]
                           (. js/document (getElementById "app")))
